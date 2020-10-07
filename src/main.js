fdbd655bf1ef0e3a250d2f6aafedf98ac606e2e2
katyaperetoka <72519898+katyaperetoka@users.noreply.github.com>
2020-10-07 22:02:13 +0300

    Basic structure complete.

---
+++ src/main.js
@@ -0,0 +1,50 @@
+document.addEventListener("keydown", function(event) {
+    if (event.code === "KeyA") {
+        let soundKeyA = new Audio("media/A.mp3");
+        soundKeyA.play();
+    }
+    else if (event.code === "KeyS") {
+        let soundKeyS = new Audio("media/S.mp3");
+        soundKeyS.play();
+    }
+    else if (event.code === "KeyD") {
+        let soundKeyD = new Audio("media/D.mp3");
+        soundKeyD.play();
+    }
+    else if (event.code === "KeyF") {
+        let soundKeyF = new Audio("media/F.mp3");
+        soundKeyF.play();
+    }
+    else if (event.code === "KeyG") {
+        let soundKeyG = new Audio("media/G.mp3");
+        soundKeyG.play();
+    }
+    else if (event.code === "KeyH") {
+        let soundKeyH = new Audio("media/H.mp3");
+        soundKeyH.play();
+    }
+    else if (event.code === "KeyJ") {
+        let soundKeyJ = new Audio("media/J.mp3");
+        soundKeyJ.play();
+    }
+    else if (event.code === "KeyW") {
+        let soundKeyJ = new Audio("media/W.mp3");
+        soundKeyJ.play();
+    }
+    else if (event.code === "KeyE") {
+        let soundKeyJ = new Audio("media/E.mp3");
+        soundKeyJ.play();
+    }
+    else if (event.code === "KeyT") {
+        let soundKeyJ = new Audio("media/T.mp3");
+        soundKeyJ.play();
+    }
+    else if (event.code === "KeyY") {
+        let soundKeyJ = new Audio("media/Y.mp3");
+        soundKeyJ.play();
+    }
+    else if (event.code === "KeyU") {
+        let soundKeyJ = new Audio("media/U.mp3");
+        soundKeyJ.play();
+    }
+});
